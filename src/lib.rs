306cbc0cde3e42dfc76aa54a092bd62aaafe07d3
Welington Dias <welingtonpdias@gmail.com>
2025-12-03 15:58:05 -0300

    First commit - Only displaying

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::* ;
+use web_sys::console::log_1 as log ;
+use wee_alloc::WeeAlloc ;
+
+#[global_allocator]
+static ALLOC : WeeAlloc = WeeAlloc::INIT ;
+
+#[wasm_bindgen]
+pub fn load_balance(balance_file: &str) {
+    //balance_file.split("\n").into_iter().for_each(|t|log(&t.replace(",", ".").into())) ;
+    balance_file.split("\n").into_iter().for_each(|t:&str|poe_no_console(&t.replace(",",".")));
+}
+
+fn poe_no_console( linha: &String) {
+    log(&linha.into()) ;
+}
